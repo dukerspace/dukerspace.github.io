9e79827ca36d2c56b6461bde8f6180eae1fed833
dukerspace <montolsaklor@gmail.com>
2022-09-02 13:56:08 +0700

    Update home page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,9 +21,6 @@ const Footer = () => (
             <li>
               <Link to="/blog">Blog</Link>
             </li>
-            <li>
-              <Link to="/about">About Me</Link>
-            </li>
           </ul>
         </Col>
       </Row>
